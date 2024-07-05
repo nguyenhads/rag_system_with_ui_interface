bc4d8f8d13540b87dcf61ce5152bcfae638b5e00
THAI HA NGUYEN <nguyenha.mse@gmail.com>
2024-07-06 00:48:23 +0900

    ADD source code for model, text splitting, building database and get the retriever

---
+++ src/const.py
@@ -0,0 +1,4 @@
+# Setup this properties at below link.
+# https://app.pinecone.io/organizations/-O-e70oiMaFGdaCjw7L3/projects/795659f5-182c-4cb5-afb9-6a18d6a466f8/indexes?sessionType=login
+INDEX_NAME = "pdf-query-app"
+NAME_SPACE = "pdf-query-app-space"
